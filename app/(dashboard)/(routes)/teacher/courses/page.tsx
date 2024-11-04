6c74ba8b2c14421cfad62ebfd810d00e1d1d18e1
AmrMustafa282 <amrdarwish115@gmail.com>
2024-11-04 09:34:44 +0200

    feat:courses_data_table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,32 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-const CoursesPage = () => {
+async function getData(): Promise<any[]> {
+ const { userId } = auth();
+ if (!userId) {
+  return redirect("/");
+ }
+ const courses = await db.course.findMany({
+  where: {
+   userId,
+  },
+  orderBy: {
+   createdAt: "desc",
+  },
+ });
+ return courses;
+}
+
+const CoursesPage = async () => {
+ const data = await getData();
  return (
   <div className="p-4">
-   <Link href={"/teacher/create"}>
-    <Button>New Course</Button>
-   </Link>
+   <DataTable columns={columns} data={data} />
   </div>
  );
 };
--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,6 +1,9 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import { isAuthorized } from "../../utils/is-authorized";
+import { isCourseOwner } from "../../utils/is-course-owner";
+import { revalidatePublishment } from "../../utils/revalidate-publishment";
 
 export async function PATCH(
  req: Request,
@@ -26,4 +29,44 @@ export async function PATCH(
   console.log("[COURSES]", error);
   return new NextResponse("Internal Error", { status: 500 });
  }
+}
+
+export async function DELETE(
+  req: Request,{params}: {params: {courseId: string;}}
+){
+  try {
+    const {userId}  = isAuthorized()
+    const {courseId} = params;
+    if(!courseId ){
+      return new NextResponse("chapterId or courseId is missing", {status: 404});
+    }
+    isCourseOwner({ courseId, userId  });
+    const course = await db.course.findUnique({
+      where: {id: courseId, userId},
+    })
+    if(!course){
+      return new NextResponse("Course not found", {status: 404});
+    }
+    
+    // TODO: Delete all chapters, muxData, and muxAssets
+    // if(chapter.videoUrl){
+    //   const muxData = await db.muxData.findFirst({
+    //     where: {chapterId},
+    //   });
+    //   if(muxData){
+    //     await video.assets.delete(muxData.assetId);
+    //     await db.muxData.delete({where: {id: muxData.id}});
+    //   }
+    // }
+
+    await db.course.delete({where: {id: courseId}});
+
+    revalidatePublishment(courseId);
+
+    return new NextResponse("Course deleted successfully", {status: 200});
+
+  } catch (error) {
+    console.log("[COURSE_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
