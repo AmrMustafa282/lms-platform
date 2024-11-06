5933cd3285192b63a53636fa0339564ec1651129
AmrMustafa282 <amrdarwish115@gmail.com>
2024-11-06 18:35:38 +0200

    feat:stripe_integration

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface CourseEnrollButtonProps {
  courseId: string;
@@ -8,8 +11,27 @@ interface CourseEnrollButtonProps {
 }
 
 const CourseEnrollButton = ({ courseId, price }: CourseEnrollButtonProps) => {
+ const [isLoading, setIsLoading] = useState(false);
+
+ const onClick = async () => {
+  try {
+   setIsLoading(true);
+   const res = await axios.post(`/api/courses/${courseId}/checkout`);
+   window.location.assign(res.data.url);
+  } catch (error) {
+   toast.error("Failed to enroll in course");
+  } finally {
+   setIsLoading(false);
+  }
+ };
+
  return (
-  <Button size={"sm"} className="w-full md:w-auto">
+  <Button
+   onClick={onClick}
+   disabled={isLoading}
+   size={"sm"}
+   className="w-full md:w-auto"
+  >
    Enroll for {formatPrice(price)}
   </Button>
  );
